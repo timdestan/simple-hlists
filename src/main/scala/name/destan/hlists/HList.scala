0c44ba8a1eb54fd867df29fbb86db5196f1ac4eb
Tim Destan <tim@destan.name>
2015-08-16 15:10:47 -0400

    Implement simple booleans and numbers.

--- src/main/scala/name/destan/hlists/HList.scala
+++ src/main/scala/name/destan/hlists/HList.scala
@@ -2,6 +2,45 @@ package name.destan.hlists
 
 import scala.language.higherKinds
 
+trait TypeLevelMagic {
+
+sealed trait Bool {
+  type If[IfTrue <: Up, IfFalse <: Up, Up] <: Up
+}
+
+type If[Condition <: Bool, IfTrue <: Up, IfFalse <: Up, Up] =
+    Condition#If[IfTrue, IfFalse, Up]
+
+sealed trait True extends Bool {
+  type If[IfTrue <: Up, IfFalse <: Up, Up] = IfTrue
+}
+
+sealed trait False extends Bool {
+  type If[IfTrue <: Up, IfFalse <: Up, Up] = IfFalse
+}
+
+sealed trait Natural {
+  type Match[IfZero <: Up, IfNonZero[N <: Natural] <: Up, Up]
+}
+
+sealed trait _0 extends Natural {
+  type Match[IfZero <: Up, IfNonZero[N <: Natural] <: Up, Up] = IfZero
+}
+
+sealed trait Successor[N <: Natural] extends Natural {
+    type Match[IfZero <: Up, IfNonZero[N <: Natural] <: Up, Up] = IfNonZero[N]
+}
+
+type _1 = Successor[_0]
+type _2 = Successor[_1]
+type _3 = Successor[_2]
+type _4 = Successor[_3]
+type _5 = Successor[_4]
+type _6 = Successor[_5]
+type _7 = Successor[_6]
+type _8 = Successor[_7]
+type _9 = Successor[_8]
+
 sealed trait HList {
   type Head
   type Tail <: HList
@@ -10,7 +49,7 @@ sealed trait HList {
   def head: Head
   def tail: Tail
   def ++[L <: HList](list: L): Append[L]
-  def ::[V](value: V) = HCons(value, this) 
+  def ::[V](value: V) = HCons(value, this)
 }
 
 case object HNil extends HList {
@@ -30,4 +69,5 @@ case class HCons[H, T <: HList](head: H, tail: T) extends HList {
 
   def ++[L <: HList](list : L): Append[L] = HCons(head, (tail ++ list))
 }
- 
+
+}
--- src/test/scala/HListSpec.scala
+++ src/test/scala/HListSpec.scala
@@ -1,7 +1,16 @@
 import org.scalatest._
 import name.destan.hlists._
 
-class HListSpec extends FlatSpec with Matchers {
+class HListSpec extends FlatSpec with Matchers with TypeLevelMagic {
+  def areEqual[A,B](implicit witness:(A =:= B) = null) = witness != null
+
+  "Booleans" should "allow If testing" in {
+    areEqual[If[True, _2, _1, Natural], _2] should be (true)
+    areEqual[If[False, _1, _2, Natural], _2] should be (true)
+    areEqual[If[True, _1, _2, Natural], _2] should be (false)
+    areEqual[If[False, _2, _1, Natural], _2] should be (false)
+  }
+
   "HNil" should "raise exception if head or tail called" in {
     a [NoSuchElementException] should be thrownBy {
       HNil.head
